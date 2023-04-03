c75a996b903e1b5cd11ea94fcfb79679ca52e374
Abduvahob <dilmurodayitboyev@gmail.com>
2023-04-03 20:42:47 +0500

    resume

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./app.scss";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="container">
+      <header>
+        <h1>Abduvohob</h1>
+        <h2>@Abdu_vohob telegram me</h2>
+        <h3>994339065 phone number me</h3>
+      </header>
+
+      <App />
+    </div>
+  </React.StrictMode>
+);
